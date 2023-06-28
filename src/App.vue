ca75bf955b58f43f049ab89b13d319b0b2142d0a
unknown <phachamowski@gmail.com>
2023-06-28 13:50:27 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-content>
+      <CalendarView />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalendarView from './components/CalendarView';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CalendarView
+  },
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import vueTextAreaAutoSize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-createApp(App).mount('#app')
+Vue.use(vueTextAreaAutoSize)
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAd3FeNKT64V5LlZ7Ie4_qLAg5Uxl_LUhM",
+    authDomain: "project-data-managment.firebaseapp.com",
+    projectId: "project-data-managment",
+    storageBucket: "project-data-managment.appspot.com",
+    messagingSenderId: "13217576467",
+    appId: "1:13217576467:web:50f8db3da45202b29f71e6"
+  })
+
+  export const db=firebase.firestore()
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
